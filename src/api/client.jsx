0e7ef1a0571bb8090374bd9a8dad13deb8a4e9c6
Mauricio Corrêa <mauriciocorrea@MacBook-Pro-de-Mauricio-2.local>
2025-05-31 21:24:00 -0300

    alterado o backend

--- src/api/client.jsx
+++ src/api/client.jsx
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
-  // baseURL: 'https://backend-production-5486.up.railway.app',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://backend-production-5486.up.railway.app',
   withCredentials: true // ← permite uso de cookies
 });
 
